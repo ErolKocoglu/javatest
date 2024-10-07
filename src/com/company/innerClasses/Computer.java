209ef9324adfbd2cf4d1f043d70ceda9d6b73c4b
ErolKocoglu <erolkocoglu44@gmail.com>
2024-10-07 22:34:53 +0300

    start learing inner classes

---
+++ src/com/company/innerClasses/Computer.java
@@ -0,0 +1,30 @@
+package com.company.innerClasses;
+
+public class Computer {
+
+    // Take variables and oeration type and print the result
+    public void compute(int var1, int var2, String operation) {
+        Cpu c = new Cpu();
+        if (operation.equals("+")) {
+            c.sum(var1, var2);
+        } else if (operation.equals("-")) {
+            c.subtract(var1, var2);
+        }
+    }
+
+
+
+    private class Cpu {
+
+        private int coreCount=1;
+        void sum(int var1, int var2) {
+            System.out.println(var1 + var2);
+        }
+
+        void subtract(int var1, int var2) {
+            System.out.println(var1 - var2);
+        }
+
+
+    }
+}
---
+++ src/com/company/innerClasses/Main.java
@@ -0,0 +1,9 @@
+package com.company.innerClasses;
+
+public class Main {
+    public static void main(String[] args) {
+        Computer c= new Computer();
+        c.compute(5,2,"+");
+        c.compute(10,2,"-");
+    }
+}
